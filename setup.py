4f18a790f0c038b6ad4641fa6c039386e68ca585
DeepSpace <deepspace2@gmail.com>
2015-08-24 16:44:02 +0300

    fixed webpage in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # long_description=long_description,
 
     # The project's main homepage.
-    # url='https://github.com/pypa/sampleproject',
+    url='https://github.com/DeepSpace2/StyleFrame',
 
     # Author details
     author='DeepSpace',
