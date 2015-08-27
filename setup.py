2e4fe2988682a7cec553e2757af38cf8b5fc9408
DeepSpace <deepspace2@gmail.com>
2015-08-27 09:41:39 +0300

    added readthedocs link to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.0.5.5',
 
-    description='A library that wraps pandas and openpyxl and allows easy styling of dataframes in excel, A library that wraps pandas and openpyxl and allows easy styling of dataframes in excel. A simple documentation can be found at https://github.com/DeepSpace2/StyleFrame',
+    description='A library that wraps pandas and openpyxl and allows easy styling of dataframes in excel, A library that wraps pandas and openpyxl and allows easy styling of dataframes in excel. A simple documentation can be found at http://styleframe.readthedocs.org/en/latest/',
     # long_description=long_description,
 
     # The project's main homepage.
