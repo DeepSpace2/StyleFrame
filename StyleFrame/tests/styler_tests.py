e872b97ce7363f9da07edfb45e39edc4eb88a0c6
Adi Vaknin <deepspace2@users.noreply.github.com>
2018-03-02 15:33:50 +0200

    - Created styler_tests - Added test for Styler.from_openpyxl_style

---
+++ StyleFrame/tests/styler_tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from StyleFrame import Styler, utils
+
+
+class StylerTests(unittest.TestCase):
+    def test_from_openpyxl_style(self):
+        styler_obj = Styler(bg_color=utils.colors.yellow, bold=True, font=utils.fonts.david, font_size=16,
+                            font_color=utils.colors.blue, number_format=utils.number_formats.date, protection=True,
+                            underline=utils.underline.double, border_type=utils.borders.double,
+                            horizontal_alignment=utils.horizontal_alignments.center,
+                            vertical_alignment=utils.vertical_alignments.bottom, wrap_text=False, shrink_to_fit=True,
+                            fill_pattern_type=utils.fill_pattern_types.gray0625, indent=1)
+
+        self.assertEqual(styler_obj,  Styler.from_openpyxl_style(styler_obj.create_style(), []))
--- StyleFrame/tests/tests.py
+++ StyleFrame/tests/tests.py
@@ -4,10 +4,11 @@ from StyleFrame.tests.commandline_tests import CommandlineInterfaceTest
 from StyleFrame.tests.container_tests import ContainerTest
 from StyleFrame.tests.series_tests import SeriesTest
 from StyleFrame.tests.style_frame_tests import StyleFrameTest
+from StyleFrame.tests.styler_tests import StylerTests
 
 
 def run():
-    test_classes = [ContainerTest, StyleFrameTest, CommandlineInterfaceTest, SeriesTest]
+    test_classes = [ContainerTest, StyleFrameTest, CommandlineInterfaceTest, SeriesTest, StylerTests]
     for test_class in test_classes:
         suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
         unittest.TextTestRunner().run(suite)
