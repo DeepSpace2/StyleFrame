ab0a25c7bb5a96ab3a4ee8839844da282479ffd9
Adi <deepspace2@gmail.com>
2020-01-30 22:37:22 +0200

    Changed StyleFrame package name to styleframe to follow PEP8

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=find_version('StyleFrame', 'version.py'),
+    version=find_version('styleframe', 'version.py'),
 
     description=DESCRIPTION,
     long_description=DESCRIPTION,
