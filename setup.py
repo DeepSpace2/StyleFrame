46aeafce9a99a15e51b4f43ed86dfa9991b9c1bd
Adi Vaknin <DeepSpace2@users.noreply.github.com>
2022-02-27 12:13:02 +0200

    Removed xlrd as a dependency

--- setup.py
+++ setup.py
@@ -82,5 +82,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['openpyxl>=2.5,<4', 'colour>=0.1.5,<0.2', 'jsonschema', 'xlrd>=1.0.0,<1.3.0', 'pandas<2']
+    install_requires=['openpyxl>=2.5,<4', 'colour>=0.1.5,<0.2', 'jsonschema', 'pandas<2']
 )
