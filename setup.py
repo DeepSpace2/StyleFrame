172c384e01086b143b2908fd559be3ab85b4f064
DeepSpace2 <deepspace2@gmail.com>
2022-07-23 18:47:16 +0300

    Added Python 3.10 support

--- setup.py
+++ setup.py
@@ -67,7 +67,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
 
     # What does your project relate to?
